dfacb1b21ddf09e0849b0aa491cfeaa338ea1b01
wegoagain-dev <tsdevelops00@gmail.com>
2025-11-23 17:34:22 +0000

    portfolio change to minimalistic

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,22 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wegoagain.dev",
   integrations: [sitemap(), mdx(), pagefind()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
   },
+
+  adapter: vercel(),
 });
